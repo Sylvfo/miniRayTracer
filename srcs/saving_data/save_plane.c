b67722983d6b50d2d8badd8782a93f9f3e201d0c
C0X23 <megret.corentingmail.com>
2025-03-08 10:10:32 +0000

    Refactor la sauvegarde des plans : extraction des fonctions de parsing pour les coordonnées, l'orientation et la couleur, et mise à jour des propriétés du plan.

--- srcs/saving_data/save_plane.c
+++ srcs/saving_data/save_plane.c
@@ -6,11 +6,45 @@
 /*   By: cmegret <cmegret@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 15:13:43 by cmegret           #+#    #+#             */
-/*   Updated: 2025/03/07 17:26:27 by cmegret          ###   ########.fr       */
+/*   Updated: 2025/03/08 09:12:37 by cmegret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../header/minirt.h"
+
+static void	parse_plane_coordinates(char **line, t_coord *coord)
+{
+	skip_whitespace((const char **)line);
+	parse_coordinates(line, &coord->x, &coord->y, &coord->z);
+	coord->t = 1;
+}
+
+static void	parse_plane_orientation(char **line, t_coord *orientation)
+{
+	skip_whitespace((const char **)line);
+	parse_coordinates(line, &orientation->x, &orientation->y, &orientation->z);
+	orientation->t = 0;
+}
+
+static void	parse_plane_color(char **line, t_color *color)
+{
+	skip_whitespace((const char **)line);
+	parse_color(line, &color->r, &color->g, &color->b);
+}
+
+static void	update_plane_properties(t_obj *plane, t_coord *coord,
+		t_coord *orientation, t_color *color)
+{
+	plane->p_coord->x = coord->x;
+	plane->p_coord->y = coord->y;
+	plane->p_coord->z = coord->z;
+	plane->color->r = color->r;
+	plane->color->g = color->g;
+	plane->color->b = color->b;
+	plane->v_axe->x = orientation->x;
+	plane->v_axe->y = orientation->y;
+	plane->v_axe->z = orientation->z;
+}
 
 void	save_plane(char *line, t_pix **pix, t_num_obj *num_obj)
 {
@@ -20,25 +54,12 @@ void	save_plane(char *line, t_pix **pix, t_num_obj *num_obj)
 	t_obj		*plane;
 
 	line += 2;
-	skip_whitespace((const char **)&line);
-	parse_coordinates(&line, &coord.x, &coord.y, &coord.z);
-	coord.t = 1;
-	skip_whitespace((const char **)&line);
-	parse_coordinates(&line, &orientation.x, &orientation.y, &orientation.z);
-	orientation.t = 0;
-	skip_whitespace((const char **)&line);
-	parse_color(&line, &color.r, &color.g, &color.b);
+	parse_plane_coordinates(&line, &coord);
+	parse_plane_orientation(&line, &orientation);
+	parse_plane_color(&line, &color);
 	plane = pix[0][0].obj[2][num_obj->plan];
 	if (!plane)
 		return ;
-	plane->p_coord->x = coord.x;
-	plane->p_coord->y = coord.y;
-	plane->p_coord->z = coord.z;
-	plane->color->r = color.r;
-	plane->color->g = color.g;
-	plane->color->b = color.b;
-	plane->v_axe->x = orientation.x;
-	plane->v_axe->y = orientation.y;
-	plane->v_axe->z = orientation.z;
+	update_plane_properties(plane, &coord, &orientation, &color);
 	num_obj->plan++;
 }
