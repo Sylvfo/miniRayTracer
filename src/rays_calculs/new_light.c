aed1a5297e879399049a6a9f37ae1831a6278785
C0X23 <megret.corentin@gmail.com>
2025-04-14 23:20:33 +0200

    Ajout de la fonction compute_specular et mise à jour de compute_pointlight pour intégrer la réflexion de la lumière. Amélioration de la gestion de l'intensité lumineuse dans light_intensity.

--- src/rays_calculs/new_light.c
+++ src/rays_calculs/new_light.c
@@ -3,14 +3,57 @@
 /*                                                        :::      ::::::::   */
 /*   new_light.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: syl <syl@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: cmegret <cmegret@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 10:14:58 by syl               #+#    #+#             */
-/*   Updated: 2025/04/10 15:03:11 by syl              ###   ########.fr       */
+/*   Updated: 2025/04/14 23:19:26 by cmegret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minirt.h"
+
+float	compute_specular(t_pix *pix, t_light *lux)
+{
+	t_coord	*view_dir;
+	t_coord	*reflect_dir;
+	float	specular_intensity;
+	float	reflect_dot_view;
+	t_coord *light_dir;
+
+	specular_intensity = 0.0;
+	// Calcul du vecteur lumière (L)
+	light_dir = normalize_vector(substraction(lux->p_world, pix->comps->p_touch));
+
+	// Calcul de la réflexion du vecteur lumière : R = 2 (N . L) N - L
+	reflect_dir = substraction(
+		scalar_mult(
+			pix->comps->v_norm_parral,
+			2.0 * dot_product(
+				pix->comps->v_norm_parral,
+				light_dir
+			)
+		),
+		light_dir
+	);
+	reflect_dir = normalize_vector(reflect_dir);
+
+	// Vecteur d'observation (inverse du vecteur vers le point d'intersection)
+	view_dir = normalize_vector(scalar_mult(pix->comps->v_norm_parral, -1.0));
+
+	// Produit scalaire entre R et V (reflect_dot_view)
+	reflect_dot_view = dot_product(reflect_dir, view_dir);
+	if (reflect_dot_view > 0)
+	{
+		// Application de shininess et factor de specular
+		specular_intensity = lux->ratio * pow(reflect_dot_view, 50.0) * 0.5;
+	}
+
+	free(view_dir);
+	free(reflect_dir);
+	free(light_dir);
+	return (specular_intensity);
+}
+
 
 void	new_light(t_pix ***pix)
 {
@@ -45,49 +88,6 @@ void ComputeLighting(t_pix *pix)
 
 	intensity = light_intensity(pix);
 	scalar_mult_color(pix->color, intensity);
-}
-
-
-float compute_pointlight(t_pix *pix, t_light *lux)
-{
-	t_coord *v_light;
-	float n_dot_l;
-	float intensity;
-
-	intensity = 0.0;
-
-	// Calcul du vecteur lumière (L)
-//	printf("Appel à substraction avec :\n");
-//	printf("lux->p_coord : x = %f, y = %f, z = %f\n", lux->p_coord->x, lux->p_coord->y, lux->p_coord->z);
-//	printf("pix->comps->p_touch : x = %f, y = %f, z = %f\n", pix->comps->p_touch->x, pix->comps->p_touch->y, pix->comps->p_touch->z);
-	v_light = substraction(lux->p_world, pix->comps->p_touch);
-//	printf("Vecteur lumière (L) : x = %f, y = %f, z = %f\n", v_light->x, v_light->y, v_light->z);
-
-	// Normalisation des vecteurs
-//	pix->comps->v_norm_parral = normalize_vector(pix->comps->v_norm_parral);
-//	printf("Normale (N) normalisée : x = %f, y = %f, z = %f\n",
-//		pix->comps->v_norm_parral->x, pix->comps->v_norm_parral->y, pix->comps->v_norm_parral->z);
-
-	v_light = normalize_vector(v_light);
-//	printf("Vecteur lumière (L) normalisé : x = %f, y = %f, z = %f\n",
-//		v_light->x, v_light->y, v_light->z);
-
-	// Calcul du produit scalaire entre la normale (N) et le vecteur lumière (L)
-	n_dot_l = dot_product(pix->comps->v_norm_parral, v_light);
-//	printf("Produit scalaire (N . L) : %f\n", n_dot_l);
-	// Vérification si le produit scalaire est positif
-	if (n_dot_l > 0)
-	{
-		intensity = lux->ratio * n_dot_l / (length_vector(pix->comps->v_norm_parral) * length_vector(v_light));
-	//	printf("Intensité calculée : %f\n", intensity);
-	}
-//	else
-//		printf("Produit scalaire négatif, aucune contribution lumineuse.\n");
-
-	// Libération de la mémoire pour le vecteur lumière
-	free(v_light);
-
-	return (intensity);
 }
 
 t_hits intersect_object(t_obj *object, t_ray *ray)
@@ -179,37 +179,92 @@ bool is_in_shadow(t_coord *point, t_light *light, t_obj ***objects)
 	return (false);
 }
 
+float compute_pointlight(t_pix *pix, t_light *lux)
+{
+	t_coord *v_light;
+	float n_dot_l;
+	float intensity;
+
+	intensity = 0.0;
+
+	// Calcul du vecteur lumière (L)
+//	printf("Appel à substraction avec :\n");
+//	printf("lux->p_coord : x = %f, y = %f, z = %f\n", lux->p_coord->x, lux->p_coord->y, lux->p_coord->z);
+//	printf("pix->comps->p_touch : x = %f, y = %f, z = %f\n", pix->comps->p_touch->x, pix->comps->p_touch->y, pix->comps->p_touch->z);
+	v_light = substraction(lux->p_world, pix->comps->p_touch);
+//	printf("Vecteur lumière (L) : x = %f, y = %f, z = %f\n", v_light->x, v_light->y, v_light->z);
+
+	// Normalisation des vecteurs
+//	pix->comps->v_norm_parral = normalize_vector(pix->comps->v_norm_parral);
+//	printf("Normale (N) normalisée : x = %f, y = %f, z = %f\n",
+//		pix->comps->v_norm_parral->x, pix->comps->v_norm_parral->y, pix->comps->v_norm_parral->z);
+
+	v_light = normalize_vector(v_light);
+//	printf("Vecteur lumière (L) normalisé : x = %f, y = %f, z = %f\n",
+//		v_light->x, v_light->y, v_light->z);
+
+	// Calcul du produit scalaire entre la normale (N) et le vecteur lumière (L)
+	n_dot_l = dot_product(pix->comps->v_norm_parral, v_light);
+//	printf("Produit scalaire (N . L) : %f\n", n_dot_l);
+	// Vérification si le produit scalaire est positif
+	if (n_dot_l > 0)
+	{
+		intensity = lux->ratio * n_dot_l / (length_vector(pix->comps->v_norm_parral) * length_vector(v_light));
+	//	printf("Intensité calculée : %f\n", intensity);
+	}
+//	else
+//		printf("Produit scalaire négatif, aucune contribution lumineuse.\n");
+
+	// Libération de la mémoire pour le vecteur lumière
+	free(v_light);
+
+	return (intensity);
+}
+
+/* float compute_pointlight(t_pix *pix, t_light *lux)
+{
+    t_coord *v_light;
+    float n_dot_l;
+    float intensity;
+
+    // Vérifier si le point d'intersection est dans l'ombre pour cette lumière
+    if (is_in_shadow(pix->comps->p_touch, lux, pix->obj))
+        return 0.0;
+
+    intensity = 0.0;
+    v_light = substraction(lux->p_world, pix->comps->p_touch);
+    v_light = normalize_vector(v_light);
+    
+    n_dot_l = dot_product(pix->comps->v_norm_parral, v_light);
+    if (n_dot_l > 0)
+    {
+        intensity = lux->ratio * n_dot_l /
+                    (length_vector(pix->comps->v_norm_parral) * length_vector(v_light));
+    }
+    free(v_light);
+    return intensity;
+} */
+
 float light_intensity(t_pix *pix)
 {
 	float intensity;
 	int i;
 
 	intensity = 0.0;
-
 	// Lumière ambiante
-	intensity += compute_ambient(pix);
-
+	intensity += pix->lux[0][0]->ratio;
 	// Lumières ponctuelles
 	i = 0;
-	while (pix->lux[1][i] != NULL) // Parcourt toutes les lumières ponctuelles
-	{
-		//if (!is_in_shadow(pix->comps->p_touch, pix->lux[1][i], pix->obj))
-		if (pix->comps->t_count > 0)
-		{
-			intensity += compute_pointlight(pix, pix->lux[1][i]);
-		}	
-		i++;
-	}
-
-	return (intensity);
+    while (pix->lux[1][i] != NULL)
+    {
+        if (pix->comps->t_count > 0)
+        {
+            // Lumière diffuse
+            intensity += compute_pointlight(pix, pix->lux[1][i]); // Problème ici
+            // Lumière spéculaire
+            intensity += compute_specular(pix, pix->lux[1][i]);
+        }
+        i++;
+    }
+    return (intensity);
 }
-
-//ok
-float 	compute_ambient(t_pix *pix)
-{
-	float i;
-
-	i = pix->lux[0][0]->ratio;
-	return (i);
-}
-
